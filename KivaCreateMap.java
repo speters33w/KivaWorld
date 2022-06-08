6b064863d207cdb3c4ac4744b8334e95ed311aba
speters33w <speters33w@gmail.com>
2022-06-08 16:08:08 -0400

    KivaCreateMap Random Map Generator complete

--- KivaCreateMap.java
+++ KivaCreateMap.java
@@ -1,6 +1,14 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.LinkedList;
 import java.util.Random;
+import java.util.Scanner;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import edu.duke.Point;
+
 
 /**
  * Creates a Kiva FloorMap
@@ -16,6 +24,26 @@ public class KivaCreateMap
     private FacingDirection facingDirection = FacingDirection.UP;
     Random random = new Random();
     private LinkedList<Point> obstacles = new LinkedList<>();
+
+    /**
+     * Returns a default map as a KivaWorld FloorMap
+     *
+     * @return default map as a FloorMap
+     */
+    public FloorMap defaultMap(){
+        FloorMap defaultMap = new FloorMap(defaultMapString());
+        return defaultMap;
+    }
+
+    /**
+     * Returns a random map as a KivaWorld FloorMap
+     *
+     * @return random map as a FloorMap
+     */
+    public FloorMap randomMap(){
+        FloorMap randomFloorMap = new FloorMap(randomMapString());
+        return randomFloorMap;
+    }
 
     /**
      * public static default floor map, returns as a String
@@ -35,23 +63,13 @@ public class KivaCreateMap
     }
 
     /**
-     * Returns a default map as a KivaWorld FloorMap
-     *
-     * @return default map as a FloorMap
-     */
-    public FloorMap defaultMap(){
-        FloorMap defaultMap = new FloorMap(defaultMapString());
-        return defaultMap;
-    }
-
-    /**
      * Create a map String that can be used with FloorMap
      *
      * @param mapWidth Width (x, col) of the map.
      * @param mapHeight Height (y, row) of the map.
      * @return The generated map in String format.
      */
-    public String randomMap(int mapWidth, int mapHeight) {
+    public String randomMapString(int mapWidth, int mapHeight) {
         StringBuilder mapFrame = new StringBuilder();
         int mapArea = mapHeight * mapWidth;
 
@@ -116,13 +134,13 @@ public class KivaCreateMap
                     mapFrame.append(" ");
                 }
 
-                Point currentPoint = new Point(row, col);
-
                 // Insert obstacles
                 for (int i = 0; i < obstacles.size()-1; i++) {
                     if (row == obstacles.get(i).getX()+1 && col == obstacles.get(i).getY()){
                         if(row != mapHeight-1) {
-                            mapFrame.replace(mapFrame.length() - 1, mapFrame.length(), "*");
+                            if (mapFrame.charAt(mapFrame.length() - 1) != '\n') {
+                                mapFrame.replace(mapFrame.length() - 1, mapFrame.length(), "*");
+                            }
                         }
                         obstacles.remove(i);
                     }
@@ -147,14 +165,59 @@ public class KivaCreateMap
      *
      * @return The generated map in String format.
      */
-    public String randomMap(){
+    public String randomMapString(){
         int mapWidth = random.nextInt(15) + 10;
         int mapHeight = random.nextInt(5) + 10;
-        return randomMap(mapWidth, mapHeight);
+        return randomMapString(mapWidth, mapHeight);
     }
 
-    public static void main(String[] args) {
+    /**
+     * Opens a JFileChooser save dialog and allows the user to save a String to a file.
+     *
+     * @param map String to be saved to the file
+     * @throws IOException if file can not be saved or written to.
+     */
+    void saveFile(String map) throws IOException {
+        File path = null;
+        JFileChooser fileChooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("FloorMap Files", "txt", "map", "maz", "maze", "fm", "FloorMap");
+        fileChooser.setFileFilter(filter);
+        fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+        fileChooser.setSelectedFile(new File("random_floor_map.txt"));
+        int option = fileChooser.showSaveDialog(null);
+        fileChooser.setVisible(true);
+        if(option == JFileChooser.APPROVE_OPTION){
+            path = new File(fileChooser.getSelectedFile().getAbsolutePath());
+            FileWriter fileWriter = new FileWriter(path);
+            fileWriter.write(map);
+            fileWriter.flush();
+            fileWriter.close();
+            System.out.println("File saved.");
+        }else {
+            System.out.println("Save canceled");
+        }
+    }
+
+    /**
+     * Returns true if input string begins with a Y or y.
+     * Y, y, yes, yepper, yup, yak and yoyo will all return true.
+     *
+     * @param yesNo String input from user
+     * @return true if input begins with a Y or y
+     */
+    public boolean yes(String yesNo){
+        return yesNo.toUpperCase().charAt(0) == 'Y';
+    }
+
+    public static void main(String[] args) throws IOException {
         KivaCreateMap kivaCreateMap = new KivaCreateMap();
-        System.out.println(kivaCreateMap.randomMap());
+        String map = kivaCreateMap.randomMapString();
+        System.out.println(map);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Save this map? (y/n)");
+        if(kivaCreateMap.yes(scanner.nextLine())){
+            kivaCreateMap.saveFile(map);
+        }
+        scanner.close();
     }
 }
